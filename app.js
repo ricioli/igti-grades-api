0bd114261363f7e7ecf1f7d76b6e835a8034f94c
Alex F Ricioli <alexricioli@outlook.com>
2020-07-07 23:35:21 -0300

    liberação de cors origin herokuapp

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:8080',
+    origin: 'https://app-grades.herokuapp.com',
   })
 );
 
