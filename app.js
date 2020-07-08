3f2e6a6af89d3ba630508d0b71a54115f40543f4
Alex F Ricioli <alexricioli@outlook.com>
2020-07-08 00:11:52 -0300

    alteração de script do procfile

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import process from 'process';
+
+process.config();
 
 import { gradeRouter } from './routes/gradeRouter.js';
 import { logger } from './config/logger.js';
